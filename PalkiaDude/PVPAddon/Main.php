d677b3a91e524bc35333826ebf6ef2e50718578c
PalkiaDude <teammegamanz@gmail.com>
2015-11-08 00:41:07 +0800

    Probably finished

--- PalkiaDude/PVPAddon/Main.php
+++ PalkiaDude/PVPAddon/Main.php
@@ -5,5 +5,17 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 class Main extends PluginBase implements Listener{
-public function onEnable(){
 
+public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN . "SUPER POWAS ACTIVATED!!");
+}
+public function Hurt(EntityDamageByEntityEvent $event){
+$killer = $event->getDamager();
+$Victim = $event->getEntity();
+if($killer->getItem()->getId() == 276 or 267){
+$event->$killer->setHealth(1);
+$event->$Victim->setOnFire(5);
+}
+}
+}
