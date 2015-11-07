e82d3241009fc64b3ff4a3fc4d9fd5c8cea2426c
PalkiaDude <teammegamanz@gmail.com>
2015-11-08 00:22:44 +0800

    Create Main.php

---
+++ PalkiaDude/PVPAddon/Main.php
@@ -0,0 +1,9 @@
+namespace PalkiaDude\PVPAddon;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+class Main extends PluginBase implements Listener{
+public function onEnable(){
+
